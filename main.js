250841f56ab53c4e9af3f134312eb0e8db88b629
Rug <noah.price.237@gmail.com>
2021-07-12 16:44:00 -0400

    Converts .OBJ in given dir to .GLTF in second dir Add output data to .gitignore

--- main.js
+++ main.js
@@ -5,22 +5,42 @@ import obj2gltf from 'obj2gltf'
 
 // probably change to cwd when converted to actual Node script
 // For some reason this pathname is the only way to make it work
-const folder = path.dirname('./models/models');
+// Either Webstorm weirdness, or my own lack of understanding.
+const __dirname = path.dirname('./models/models');
+const outDir = path.join(__dirname, 'GLTF Out');
+
+// Create output folder
+fs.mkdir(outDir,
+    {recursive: true},
+    (err) => {
+    if (err){
+        console.log(err);
+    }
+});
 
 // Read all files in dir
-const files = fs.readdirSync(folder);
-
+const files = fs.readdirSync(__dirname);
 // Select required files
 const targetFiles =  files.filter(file => {
     return path.extname(file).toLowerCase() === ".obj";
 })
-console.log(targetFiles)
 
-// convert OBJ to GLTF
-// for (const file in targetFiles){
-//     obj2gltf(file)
-//         .then(function (gltf){
-//             const data = Buffer.from(JSON.stringify(gltf));
-//             fs.writeFileSync(file, data);
-//         });
-// }
+// Change from .obj to .gltf
+// Function created for ease of use, whole script needs cleanup
+function changeExtension(file, extension){
+    const basename = path.basename(file, path.extname(file))
+    return path.join(path.dirname(file), basename + extension);
+}
+//convert OBJ to GLTF
+for (const file in targetFiles){
+    const fn = path.join(__dirname, targetFiles[file]);
+    const fn_out = path.join(outDir, changeExtension(targetFiles[file], ".gltf"));
+    const ext = ".gltf";
+
+    obj2gltf(fn)
+        .then(function (gltf){
+            const data = Buffer.from(JSON.stringify(gltf));
+            fs.writeFileSync(fn_out, data);
+        });
+    console.log("Files written to " + outDir)
+}

31cf570a2517e5e9449f46a59329a0d57e861bc7
Rug <noah.price.237@gmail.com>
2021-07-13 16:36:22 -0400

    Remove space in outDir Change output to be binary .glb Merge n scenes from all frames into single scene with n nodes
    Writing out fails, too many buffers.

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ import {Document, NodeIO} from '@gltf-transform/core';
 import path from "path";
 import fs from 'fs';
 import obj2gltf from 'obj2gltf'
+import {prune} from "@gltf-transform/functions";
 
 // probably change to cwd when converted to actual Node script
 // For some reason this pathname is the only way to make it work
 // Either Webstorm weirdness, or my own lack of understanding.
 const __dirname = path.dirname('./models/models');
-const outDir = path.join(__dirname, 'GLTF Out');
+const outDir = path.join(__dirname, 'GLTFOut');
 
 // Create output folder
 fs.mkdir(outDir,
@@ -32,26 +33,53 @@ function changeExtension(file, extension){
     return path.join(path.dirname(file), basename + extension);
 }
 //convert OBJ to GLTF
-// for (const file in targetFiles){
-//     const fn = path.join(__dirname, targetFiles[file]);
-//     const fn_out = path.join(outDir, changeExtension(targetFiles[file], ".gltf"));
-//     const ext = ".gltf";
-//
-//     obj2gltf(fn)
-//         .then(function (gltf){
-//             const data = Buffer.from(JSON.stringify(gltf));
-//             fs.writeFileSync(fn_out, data);
-//         });
-//     //console.log("Files written to " + outDir)
-//}
+for (const file in targetFiles){
+    const fn = path.join(__dirname, targetFiles[file]);
+    const fn_out = path.join(outDir, changeExtension(targetFiles[file], ".glb"));
+
+    obj2gltf(fn ,{binary : true})
+        .then(function(glb){
+            //const data = Buffer.from(JSON.stringify(glb));
+            fs.writeFileSync(fn_out, glb);
+        });
+    //console.log("Files written to " + outDir)
+}
 
 // GLTF-Transform
 const io = new NodeIO();
 const document = new Document();
 
-const gltf_files = fs.readdirSync(outDir)
-console.log(io.read('models/GLTF Out/Cone.gltf'))
-// for (const file in gltf_files){
-//     const fp = path.join(outDir, gltf_files[file]);
-//     console.log(io.read(fp))
-// }
+const glb_files = fs.readdirSync(outDir).filter((file => {
+    return path.extname(file).toLowerCase() === ".glb";
+}))
+
+// Load N files and merge into one document with N Scenes
+for (const file in glb_files){
+    const fp = path.join(outDir, glb_files[file]);
+    document.merge(io.read(fp))
+}
+
+
+
+
+const root = document.getRoot();
+const mainScene = root.listScenes()[0];
+// Iterate over scenes in document, move children into common scene
+let i = 0; // frame index
+for (const scene of root.listScenes()) {
+
+    if (scene === mainScene)
+        scene.setName("Main")
+
+    for (const child of scene.listChildren()){
+        scene.setName(`Frame_${i}`)
+        mainScene.addChild(child)
+        i++
+    }
+    scene.dispose();
+}
+
+await document.transform(prune());
+
+io.write(path.join(outDir, 'model.gltf'), document)
+//console.log(mainScene.getName());

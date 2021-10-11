813ddd32ec11f759a1846618b21a6dcca75db3b6
Noah Price <noah.price.237@gmail.com>
2021-10-11 19:58:12 -0400

    Refactor `main` into reusable modules. Remove loop from `Converter`, now assumes single files. Loop moved `merge` Add `commander` and `inquirer`

---
+++ bin/index.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+
+import {program} from "commander";
+import {merge} from "../lib/commands/merge.js"
+
+
+program
+    .command('merge <format>')
+    .description('Merge all .OBJ files in a directory into a single .GLTF/.GLB file')
+    .action(merge)
+
+program.parse()

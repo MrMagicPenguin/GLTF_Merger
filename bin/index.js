7c06a40cb314d7ba3faf0446b4d0a31e1538f829
Noah Price <noah.price.237@gmail.com>
2021-10-21 23:35:42 -0400

    Add Merge ability Add Inquirer prompt

--- bin/index.js
+++ bin/index.js
@@ -3,11 +3,30 @@
 import {program} from "commander";
 // import inquirer from 'inquirer'
 import {merge} from "../lib/commands/merge.js"
+import {seqMerge} from "../lib/commands/seq-merge.js";
+import {prompt} from "../lib/commands/prompt.js";
 
 
 program
-    .command('merge <format>')
+    .command('merge <format> [inDir] [outDir] [delConversion]')
+    .option('-d, --dry-run')
+    .option('-dc, --delete-conversion')
+    .alias('m')
     .description('Merge all .OBJ files in a directory into a single .GLTF/.GLB file')
-    .action(merge)
+    .action((format, inDir, outDir) => {
+        merge(format, inDir, outDir).then(r => console.log("Merge complete!"))
+        })
 
+program
+    .command('seq-merge <format> <inDir> <outDir> <delConversion>')
+    .alias('sm')
+    .description('Merge all .OBJ files in a directory into a single .GLTF/.GLB file with flipbook animation')
+    .action(seqMerge)
+// Interactive prompt for conversion
+
+program
+    .command('prompt')
+    .alias('p')
+    .description('Interactive prompt for conversion options')
+    .action(prompt)
 program.parse()

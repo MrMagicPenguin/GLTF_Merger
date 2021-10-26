b8ba016fe474f7419c1f940b168f2461a29cb049
Noah Price <noah.price.237@gmail.com>
2021-10-26 19:19:29 -0400

    Change default input dir to CWD Add required option '--pattern'. Planned usage is to identify files for conversion, currently used as default for `--filename` flag. Add proper support for custom in/out directory arguments
    Remove TODO regarding Typing as it is

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,6 @@
 import {program} from "commander";
 // import inquirer from 'inquirer'
 import {merge} from "../lib/commands/merge.js"
-import {seqMerge} from "../lib/commands/seq-merge.js";
 import {prompt} from "../lib/commands/prompt.js";
 
 
@@ -11,8 +10,9 @@ program
     .command('merge <format> [inDir] [outDir]')
     .option('-d, --dry-run')
     .option('-s, --sequence', 'Convert output to flipbook animation')
-    .option('-dc, --delete-conversion', 'Delete all converted frames')
-    .option('-fn, --filename <name>', 'Name of output file')
+    .option('-dc, --delete-conversion', 'Delete all converted frames, default true')
+    .option('-fn, --filename <name>', 'Name of output file, defaults to Pattern name')
+    .requiredOption('-p, --pattern <pattern>', 'File Sequence pattern (e.g. "ObjectName_"')
     .alias('m')
     .description('Merge all .OBJ files in a directory into a single .GLTF/.GLB file')
     .action((format, inDir, outDir, options) => {

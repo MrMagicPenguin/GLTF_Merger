790139552183ea3f3e5778b54a78b8123382c91e
voodoo <nprice@pricemail.ca>
2021-11-01 23:14:07 -0400

    Add progress bar to console
    Using cli-progress

--- bin/index.js
+++ bin/index.js
@@ -1,39 +1,39 @@
 #! /usr/bin/env node
 
-import { program } from "commander";
+import {program} from "commander";
 // import inquirer from 'inquirer'
-import { merge } from "../lib/commands/merge.js";
-import { prompt } from "../lib/commands/prompt.js";
+import {merge} from "../lib/commands/merge.js";
+import {prompt} from "../lib/commands/prompt.js";
 
 program
-  .command("merge <format> [inDir] [outDir]")
-  // Add FPS flag
-  .option("-s, --sequence [fps]", "Convert output to flipbook animation")
-  .option(
-    "-dc, --delete-conversion",
-    "Delete all converted frames, default true"
-  )
-  .option(
-    "-fn, --filename <name>",
-    "Name of output file, defaults to Pattern name"
-  )
-  .alias("m")
-  .description(
-    "Merge all .OBJ files in a directory into a single .GLTF/.GLB file"
-  )
-  .action((format, inDir, outDir, options) => {
-    merge(format, inDir, outDir, options).then(() =>
-      console.log("Merge complete!")
-    );
-  });
+    .command("merge <format> [inDir] [outDir]")
+    // TODO use FPS flag
+    .option("-s, --sequence [fps]", "Convert output to flipbook animation")
+    .option(
+        "-dc, --delete-conversion",
+        "Delete all converted frames, default true"
+    )
+    .option(
+        "-fn, --filename <name>",
+        "Name of output file, defaults to Pattern name"
+    )
+    .alias("m")
+    .description(
+        "Merge all .OBJ files in a directory into a single .GLTF/.GLB file"
+    )
+    .action((format, inDir, outDir, options) => {
+        merge(format, inDir, outDir, options).then(() =>
+            console.log("Merge complete!")
+        );
+    });
 
 // Interactive prompt for conversion
 program
-  .command("prompt")
-  .alias("p")
-  .description("Interactive prompt for conversion options")
-  .action(() => {
-    prompt();
-  });
+    .command("prompt")
+    .alias("p")
+    .description("Interactive prompt for conversion options")
+    .action(() => {
+        prompt();
+    });
 
 program.parse();
